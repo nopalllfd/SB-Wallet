0a0a4f696a95cf8231e3ecf7b8d34b91b2af2c4d
nopalllfd <Naufalfadhilah689@gmail.com>
2026-04-04 13:05:54 +0700

    chore(auth): reduce the padding in mobile responsive

--- src/Fragments/Auth/SocialsLogin.jsx
+++ src/Fragments/Auth/SocialsLogin.jsx
@@ -6,12 +6,12 @@ function SocialsLogin() {
       <span className="google-button">
         <Button
           border={'border'}
-          className="px-18 py-4 cursor-pointer md:py-2 md:flex md:items-center md:justify-center md:gap-2 rounded-3xl border-gray-400"
+          className="max-sm:px-14 max-sm:py-2 px-18 py-4 cursor-pointer md:py-2 md:flex md:items-center md:justify-center md:gap-2 rounded-3xl border-gray-400"
         >
           <img
             src="/assets/inputs/google.svg"
             alt="google icon"
-            className="w-6"
+            className="w-12 md:w-8"
           />
           <p className="hidden md:inline text-gray-700">Sign In With Google</p>
         </Button>
@@ -19,12 +19,12 @@ function SocialsLogin() {
       <span className="facebook-button ">
         <Button
           border={'border'}
-          className="px-18 py-4 cursor-pointer md:py-2 md:flex md:items-center md:justify-center md:gap-2 rounded-3xl border-gray-400"
+          className="max-sm:px-14 max-sm:py-2 px-18 py-4 cursor-pointer md:py-2 md:flex md:items-center md:justify-center md:gap-2 rounded-3xl border-gray-400"
         >
           <img
             src="/assets/inputs/facebook.svg"
             alt="facebook icon"
-            className="w-6"
+            className="w-12 md:w-8"
           />
           <p className="hidden md:inline text-gray-700">Sign In With Facebook</p>
         </Button>
