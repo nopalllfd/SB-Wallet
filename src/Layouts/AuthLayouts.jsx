033f48d0fea382266e36575195a4026c88a5e968
nopalllfd <Naufalfadhilah689@gmail.com>
2026-06-03 09:37:34 +0700

    fix: protected route

--- src/Layouts/AuthLayouts.jsx
+++ src/Layouts/AuthLayouts.jsx
@@ -1,3 +1,15 @@
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router';
+
 export const AuthLayouts = ({ children }) => {
+  const auth = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (auth.isAuthenticated) {
+      navigate('/dashboard');
+    }
+  }, [auth.isAuthenticated, navigate]);
   return <section className="w-full max-w-2xl py-10 px-4 flex flex-col gap-4 bg-white rounded-xl md:rounded-xl md:p-10">{children}</section>;
 };
