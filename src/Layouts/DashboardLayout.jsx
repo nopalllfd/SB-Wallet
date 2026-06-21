091fb11750fa24f762abef28e0dde4e11e4623aa
nopalllfd <Naufalfadhilah689@gmail.com>
2026-06-21 19:50:38 +0700

    feat: change header dashboard color

--- src/Layouts/DashboardLayout.jsx
+++ src/Layouts/DashboardLayout.jsx
@@ -34,7 +34,7 @@ export const DashboardLayout = ({ children, locationDetail }) => {
 
   return (
     <div className="min-h-screen bg-gray-50 md:grid md:grid-rows-[auto_1fr]">
-      <header className="hidden md:grid md:grid-cols-[16rem_1fr] lg:grid-cols-[18rem_1fr] bg-white border-b border-gray-200">
+      <header className="hidden md:grid md:grid-cols-[16rem_1fr] lg:grid-cols-[18rem_1fr] bg-blue-500 border-b border-gray-200">
         <div className="px-6 py-3 flex items-center">
           <BrandHeader />
         </div>
