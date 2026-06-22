937c3689d753d69e40ef0a4d576215c3fc396fae
nopalllfd <Naufalfadhilah689@gmail.com>
2026-06-22 10:13:04 +0700

    feat: delete brand header for mobile

--- src/components/NavMenuItems/index.jsx
+++ src/components/NavMenuItems/index.jsx
@@ -1,15 +1,17 @@
 import { NavLink } from 'react-router';
-import { Button } from '../Button';
 
-export const NavMenuItem = (props) => {
+export const NavMenuItem = ({ to, children }) => {
   return (
-    <Button buttonTextColor="text-blue-500 pb-5 text-md">
-      <NavLink
-        to={props.to}
-        className={({ isActive }) => (isActive ? 'bg-blue-700 text-white px-3 py-2 rounded-md w-full' : 'text-gray-700 px-3 py-2 rounded-md')}
-      >
-        {props.children}
-      </NavLink>
-    </Button>
+    <NavLink
+      to={to}
+      className={({ isActive }) =>
+        `
+        block w-full px-4 py-3 rounded-md transition-all duration-200
+        ${isActive ? 'bg-blue-700 text-white' : 'text-gray-700 hover:bg-gray-100'}
+      `
+      }
+    >
+      {children}
+    </NavLink>
   );
 };
